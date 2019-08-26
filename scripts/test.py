9b0c6e0353e78c1b9368c01e8e1c5f1894092aff
JoelStansbury <48299585+JoelStansbury@users.noreply.github.com>
2019-08-26 08:02:22 -0400

    Added basic file structure and a few files

---
+++ scripts/test.py
@@ -0,0 +1,12 @@
+import cv2
+
+
+
+# load the required trained XML classifiers
+# https://github.com/Itseez/opencv/blob/master/
+# data/haarcascades/haarcascade_frontalface_default.xml
+# Trained XML classifiers describes some features of some
+# object we want to detect a cascade function is trained
+# from a lot of positive(faces) and negative(non-faces)
+# images.
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
